b4362609d58a0baee45a98c1bcf87b62319132ad
MarinaGrudkovskaya <mgg1005@yandex.ru>
2023-12-03 08:53:30 +0300

    лекция 4

---
+++ TowArray_1/Program.cs
@@ -0,0 +1,62 @@
+﻿// // строковый двумерный массив
+
+// // string[,] table = new string[2, 5];
+// // // String.Empty   -  инициализация строк
+// // // table[0,0] table[0,1] table[0,2]  table[0,3]  table[0,4]
+// // // table[1,0] table[1,1] table[1,2]  table[1,3]  table[1,4]
+
+// // table[1,2] = "слово";
+
+// // for (int rows = 0; rows < 2; rows++)
+// // {
+// //    for (int columns = 0; columns < 5; columns++)
+// //    {
+// //     Console.WriteLine($"-{table[rows, columns]}-");
+// //    } 
+// // }
+
+// // численный двумерный массив
+
+// int[,] matrix = new int[3,4];
+
+// for (int i = 0; i < matrix.GetLength(0); i++)     // через "GetLength(0)" указали кол.строк
+// {
+//     for (int j = 0; j < matrix.GetLength(1); j++) // через "GetLength(1)" указали кол.столбцов
+//     {
+//         Console.Write($"{matrix[i,j]} ");
+//     }
+//     Console.WriteLine();
+// }
+
+// метод печатающий двумерный массив:
+void PrintArray(int[,] matr)
+{
+for (int i = 0; i < matr.GetLength(0); i++)    
+{
+    for (int j = 0; j < matr.GetLength(1); j++) 
+    {
+        Console.Write($"{matr[i,j]} ");
+    }
+    Console.WriteLine();
+}
+}
+// PrintArray(matrix);
+
+// метод заполняющий двумерный массив
+
+void FillArray(int[,] matr)
+  {
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            matr[i,j] = new Random().Next(10,100);
+        }
+    }
+  }
+
+  int[,] matrix = new int[3,4];
+  PrintArray(matrix);
+  Console.WriteLine();
+  FillArray(matrix);
+  PrintArray(matrix);
---
+++ TowArray_Rec/Program.cs
@@ -0,0 +1,30 @@
+﻿// Р Е К У Р С И Я(продолжение)
+// Факториал: 5! = 5 х 4 х 3 х 2 х 1
+      //      5 х 4!
+      //          4 х 3!
+      //              3 х 2!     и т д
+
+// double Factorial(int n)
+//   {
+//    if(n == 1) return 1;
+//    else return n * Factorial(n - 1);
+//   }
+// Console.WriteLine(Factorial(40));  
+// Числа Фибоначи {1 1 2 3 5 8 13 21 ....}
+// f(1) = 1
+// f(2) = 1
+// f(3) = 2
+// f(n) = f(n-1) + f(n-2)
+
+int Fibonacci(int n)
+  {
+    if (n ==1 || n == 2) return 1;
+    else return  Fibonacci(n-1) + Fibonacci(n-2);
+  }
+for (int i = 1; i < 10; i++)
+{
+  Console.WriteLine(Fibonacci(i));
+}  
+
+
+
